be02d3a61c972b93041ef324d64847cae38fe350
Vishal Kuo <vfkuo@uwaterloo.ca>
2015-08-10 18:09:52 -0400

    gitscrub

--- home/controllers/controller.js
+++ home/controllers/controller.js
@@ -1,5 +1,12 @@
 var app = angular.module('app', [])
 app.controller('cardController', ['$scope', function($scope){
     $scope.repos = data
-    console.log($scope.repos)
+    for (repo in data){
+        for (header in data[repo]['headers']){
+            aboutObj = data[repo]['headers'][header]
+            if (aboutObj['header'] === "About"){
+                data[repo]['about'] = aboutObj['textgi']
+            }
+        }
+    }
 }])
