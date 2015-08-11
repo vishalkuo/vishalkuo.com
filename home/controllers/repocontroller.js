21a8c69919fcd98c7402802d2aac23c3a35f2909
Vishal Kuo <vfkuo@uwaterloo.ca>
2015-08-11 18:50:15 -0400

    Added repo data

--- home/controllers/repocontroller.js
+++ home/controllers/repocontroller.js
@@ -1,5 +1,16 @@
 var repoApp = angular.module('repoApp', [])
-app.controller('repoController', ['$scope', function($scope){
+repoApp.controller('repoController', ['$scope', function($scope){
     $scope.repos = data
+    title = window.location.href.split('#')[1]
+    for (repo in data){
+        if (data[repo]['title'] === title){
+            $scope.repo = data[repo]
+        }
+    }
 
+    for (obj in $scope.repo['headers']){
+        if ($scope.repo['headers'][obj].header === 'About'){
+            $scope.about = $scope.repo['headers'][obj]
+        }
+     }
 }])
