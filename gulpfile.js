208dea5a2f6e2571cd20535fa5c1744b24864bda
Vishal Kuo <vfkuo@uwaterloo.ca>
2015-08-19 17:02:52 -0400

    gulp start

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+/**
+ * Created by vishalkuo on 15-08-19.
+ */
+var gulp = require('gulp'), gutil = require('gulp-util'), uglify = require('uglify'), concat = require('concat')
+
+gulp.task('controllers', function(){
+    gulp.src('./home/js/')
+})
