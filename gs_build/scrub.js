237caab97f621014731589c0668d9b473b0dd6ab
Vishal Kuo <vfkuo@uwaterloo.ca>
2015-08-25 20:32:47 -0400

    Add dinnerBell

--- gs_build/scrub.js
+++ gs_build/scrub.js
@@ -2,21 +2,20 @@ var gitscrub = require('gitscrub')
 var secret = require('./secret')
 
 
-gitscrub.authenticate(secret.username, secret.password, function(){
-    gitscrub.setSortOptions({'custom': [
-        'FuturesRevealed',
-        'gitScrub',
-        'pasteDump',
-        'codeStats',
-        'arduinoBrakelightBackpack',
-        'Grumble',
-        'Summon (A.K.A. Genie)',
-        'dotDash',
-        'dinnerBell',
-        'StarRush']})
-    gitscrub.scrubADubDub(secret.username, secret.password, null, function(result, err){
-        gitscrub.customSort(result, function(done){
-            console.log('data = ' + JSON.stringify(done,null, 1))
-        })
+gitscrub.setSortOptions({'custom': [
+    'FuturesRevealed',
+    'gitScrub',
+    'pasteDump',
+    'codeStats',
+    'arduinoBrakelightBackpack',
+    'Grumble',
+    'Summon (A.K.A. Genie)',
+    'dotDash',
+    'dinnerBell',
+    'StarRush']})
+gitscrub.scrubADubDub(secret.username, secret.password, null, function(result, err){
+    gitscrub.customSort(result, function(done){
+        console.log('data = ' + JSON.stringify(done,null, 1))
     })
 })
+
